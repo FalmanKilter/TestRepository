7fc743664a0dc407fed42c80eeae1a2b83f58557
FalmanKilter <alex.mech.osu@gmail.com>
2021-01-22 11:39:04 +0300

    - updated function to overload the function call

--- sum_of_three.m
+++ sum_of_three.m
@@ -2,10 +2,18 @@ function [sum] = sum_of_three(arg1,arg2,arg3)
 % the function returns the sum of three input args
 
 % default value of arg3
-if nargin<3
-    arg3 = 0;
+switch nargin
+    case 0
+        arg1=0;
+        arg2=0;
+        arg3=0;
+    case 1
+        arg2=0;
+        arg3=0;
+    case 2
+        arg3=0;
 end
-
+    
 sum = arg1 + arg2 + arg3;
 
 end
